041e23ed5c9142ea4b97c96a2ceda46c4d5a68ab
ivanliu0527-cpu <ivanliu0527@gmail.com>
2025-12-17 04:46:27 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title{"Retail Business Dashboard")
+st.title("Retail Business Dashboard")
 st.header("Manager Input Section")
 st.write("Please enter the monthly sales target and select the region")
 
